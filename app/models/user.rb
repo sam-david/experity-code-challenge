adca439a4910213322660b336398ccfe65973dd8
Sam David <Sam.David.Engineer@gmail.com>
2022-04-13 14:28:11 -0700

    user httparty gem for request. Adds update tasks to user model for now

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,16 @@
 class User < ApplicationRecord
   has_many :tasks
+
+  def self.update_tasks
+    external_tasks = HTTParty.get('https://jsonplaceholder.typicode.com/todos')
+
+    external_tasks.each do |external_task|
+      Task.where(external_id: external_task['id']).first_or_create do |task|
+        user = User.where(external_id: external_task['userId']).first_or_create
+        task.user_id = user.id
+        task.title = external_task['title']
+        task.completed = external_task['completed']
+      end
+    end
+  end
 end
